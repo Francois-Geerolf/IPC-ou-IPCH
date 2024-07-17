e3ecf03bd5c3dabca21c050f9c15ce03ce4f226a
François Geerolf <100227019+Francois-Geerolf@users.noreply.github.com>
2024-07-17 10:48:41 +0200

    update FP

--- figureA2.R
+++ figureA2.R
@@ -1,35 +1,86 @@
-rm(list = ls())
+library(tidyverse)
 
-figure1_idbanks <- c("001762353", "001762354", "001762357", "001762360",
-                     "001766350", "001766351", "001766355")
+IPC_ou_IPCH_idbanks <- tribble(~ IDBANK, ~ IPC_ou_IPCH,
+                               "001759970", "IPC", # IPC mensuel
+                               "001759971", "IPCH" # IPCH mensuel
+)
 
-figure1 <- paste(figure1_idbanks, collapse = "+") |>
-  paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", i = _) |>
-  rsdmx::readSDMX() |>
-  tibble::as_tibble() |>
-  mutate(TITLE_FR = stringr::str_replace(TITLE_FR, "Pondérations de l'indice des prix à la consommation - Base 2015 - Ensemble des ménages - France - Nomenclature Coicop : ", ""),
-         TITLE_FR = stringr::str_replace(TITLE_FR, "Pondérations de l'indice des prix à la consommation harmonisé - Base 2015 - Ensemble des ménages - France - Nomenclature Coicop : ", ""))  |>
-  dplyr::transmute(Coicop2016 = TITLE_FR, TIME_PERIOD, OBS_VALUE, 
-                   `IPC ou IPCH ?` = dplyr::case_when(IDBANK %in% c("001766350", "001766351", "001766355") ~ "IPC",
-                                                      IDBANK %in% c("001762353", "001762354", "001762357", "001762360") ~ "IPCH")) |>
-  dplyr::mutate(OBS_VALUE = as.numeric(OBS_VALUE))
+min_date <- as.Date("1999-01-01")
+max_date <- as.Date("2024-06-01")
 
-saveRDS(figure1, file = "figure1.rds")
+indicefp <- fread("https://www.ipp.eu/wp-content/themes/ipp/baremes/marche-du-travail/remuneration_dans_fonction_publique/indicefp/indicefp.csv") |>
+  mutate(date = as.Date(date)) |>
+  arrange(desc(date)) |>
+  # Add today's date to be equal ------
+slice(1, 1:n()) |>
+  mutate(date = ifelse(row_number() == 1, Sys.Date(), date)) |>
+  mutate(date = as.Date(date))
 
-figure1 <- readRDS("figure1.rds")
 
-figure1 %>%
-  mutate(date = paste0(TIME_PERIOD, "-01-01"),
-         date = as.Date(date))  |>
-  filter(date >= as.Date("1996-01-01")) |>
-  arrange(date) |>
-  ggplot() + ylab("") + xlab("") + theme_minimal() +
-  geom_line(aes(x = date, y = OBS_VALUE/10000, color =  Coicop2016, linetype = `IPC ou IPCH ?`)) +
-  scale_x_date(breaks = seq(1920, 2100, 2) %>% paste0("-01-01") %>% as.Date,
+figureA2_idbanks <- tribble(~ IDBANK, ~ brut_ou_net,
+                           "001572130", "indice brut", # https://www.insee.fr/fr/statistiques/serie/001572130
+                           "001572134", "indice net"   # https://www.insee.fr/fr/statistiques/serie/001572134
+)
+
+IPC_ou_IPCH <- IPC_ou_IPCH_idbanks |>
+  mutate(data = map(IDBANK, ~ paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", .) |>
+                      rsdmx::readSDMX() |>
+                      as_tibble() |>
+                      transmute(date = as.Date(paste0(TIME_PERIOD, "-01")),
+                                OBS_VALUE = as.numeric(OBS_VALUE)))) |>
+  unnest(cols = c(data)) |>
+  select(-IDBANK) |>
+  spread(IPC_ou_IPCH, OBS_VALUE)
+
+net_brut_mensuel <- figureA2_idbanks |>
+  mutate(data = map(IDBANK, ~ paste0("https://www.bdm.insee.fr/series/sdmx/data/SERIES_BDM/", .) |>
+                      rsdmx::readSDMX() |>
+                      as_tibble()  |> 
+                      transmute(date = as.Date(zoo::as.yearqtr(TIME_PERIOD, format = "%Y-Q%q")),
+                                OBS_VALUE = as.numeric(OBS_VALUE)))) |>
+  unnest(cols = c(data)) |>
+  select(-IDBANK) |>
+  spread(brut_ou_net, OBS_VALUE) |>
+  transmute(date, net_brut = `indice net`/`indice brut`) |>
+  slice(1, 1:n(), n()) |>
+  mutate(date = case_when(row_number() == n() ~ Sys.Date(),
+                          row_number() == 1 ~ min_date,
+                          TRUE ~ date)) |>
+  complete(date = seq.Date(min(date), max(date), by = "month")) |>
+  fill(net_brut)
+
+
+figureA2 <- indicefp |>
+  select(date, value = point_indice_en_euros) |>
+  arrange(desc(date)) |>
+  complete(date = seq.Date(min(date), max(date), by = "day")) |>
+  fill(value) |>
+  left_join(IPC_ou_IPCH, by = "date") |>
+  filter(day(date) == 1) |>
+  left_join(net_brut_mensuel, by = "date") |>
+  filter(date >= min_date,
+         date <= max_date) |>
+  transmute(date,
+            IPCH = (value/value[1])*(IPCH[1]/IPCH)*net_brut,
+            IPC = (value/value[1])*(IPC[1]/IPC)*net_brut) |>
+  gather(variable, OBS_VALUE, -date)
+
+figureA2 |>
+  ggplot() + geom_line(aes(x = date, y = OBS_VALUE, color = variable)) + theme_minimal() +
+  scale_x_date(breaks = as.Date(paste0(c(seq(1999, 2100, 5), seq(1997, 2100, 5)), "-01-01")),
                labels = date_format("%Y")) +
-  theme(legend.position = c(0.78, 0.8),
+  theme(legend.position = c(0.43, 0.12),
         legend.title = element_blank()) +
-  scale_y_continuous(breaks = 0.01*seq(0, 300, .1),
-                     labels = percent_format(accuracy = .1))
+  scale_y_log10(breaks = seq(1, 0.02, -0.02),
+                labels = percent(seq(1, 0.02, -0.02)-1, acc = 1)) + 
+  ylab("") + xlab("") +
+  geom_text(data = figureA2 |> 
+              filter((year(date) %in% seq(2009, 2019, 5) & month(date) == 1)),
+            aes(x = date, y = OBS_VALUE, label = percent(OBS_VALUE-1, acc = 0.1)), 
+            fontface ="bold", color = "black", size = 3) +
+  geom_label(data = filter(figureA2, date == max(date)),
+             aes(x = date, y = OBS_VALUE, label = percent(OBS_VALUE-1, acc = 0.1), color = variable), 
+             fontface ="bold", size = 3)
 
-ggsave("figure1.png", width = 1.25*6, height = 1.25*3.375)
+ggsave("figureA2.png", width = 1.25*6, height = 1.25*3.375)
+ggsave("figureA2.pdf", width = 1.25*6, height = 1.25*3.375)
